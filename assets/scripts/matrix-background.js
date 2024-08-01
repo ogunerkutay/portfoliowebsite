a141852946a42087ba74ea0a39e2d8b70f32c077
Ogün Erkutay <77350593+ogunerkutay@users.noreply.github.com>
2024-08-01 17:25:22 +0300

    matrix updated

--- assets/scripts/matrix-background.js
+++ assets/scripts/matrix-background.js
@@ -3,7 +3,7 @@ function createMatrixBackground() {
     background.className = 'matrix-background';
     document.body.appendChild(background);
 
-    const characters = '日月水火木金土天地人子母父年中国大小ABCDEFGHIJKLMNOPQRSTUVWXYZабвгдежзийклмнопрстуфхцчшщъыьэюяΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ0123456789';
+    const characters = '日月水火木金土天地人子母父年中国大小ABCDEFGHIJKLMNOPQRSTUVWXYZабвгдежзийклмнопрстуфхцчшщъыьэюяΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ0123456789ابتثجحخدذرزسشصضطظعغفقكلمنهوي';
     const colors = ['#ff0000','#00ff00', '#0000ff', '#00ffff'];
 
     const columnCount = Math.floor(window.innerWidth / 20);
